1908b75d35ddb25a6642057589cfef86809f47b6
becca <rebecca.kaci@gmail.com>
2021-11-16 11:32:06 +0100

    Composant popular terminé

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 
 class Card extends Component {
     render() {
-        const { poster, title, release_date, overview } = this.props
+        const { poster, title, releaseDate, overview } = this.props
 
         return (
-            <div>
-                <h1>{title}</h1>
-                <img src={poster} />
-                <p>{release_date}</p>
-                <p>{overview}</p>
+            <div className="col-lg-3 col-md-6 mb-5">
+                <div class="card">
+                    <img src={(`https://image.tmdb.org/t/p/w300/${poster}`)} class="card-img-top" alt="Movie Poster" />
+                    <div class="card-body">
+                        <h4 class="card-title text-center fw-bold mb-2">{title}</h4>
+                        <p class="card-text fst-italic text-center">Realised on the : {releaseDate}</p>
+                        <p class="card-text"><span className="fw-bold">Description :</span> {overview}</p>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -12,29 +12,36 @@ class Popular extends Component {
     }
 
     componentDidMount(){
-        fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key={74ff4d5b18f55c304a239fadf716fe2f}`)
-        .then(response => response.json)
+        fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=74ff4d5b18f55c304a239fadf716fe2f`)
+        .then(response => response.json())
         .then(result => {
-            this.setState({movies: result})
+            console.log(result);
+            this.setState({movies: result.results})
         })
+        console.log(this.state.movies);
     }
 
     render() {
+        console.log(this.state.movies);
         const { movies } = this.state
 
         return (
-            <div>
-                <h1>Popular</h1>
-
-                {movies.map(movie => (
-                    <Card 
-                        poster={movie.poster}
-                        title={movie.title}
-                        release_date={movie.release_date}
-                        overview={movie.overview}
-                    />
-                ))}
-            </div>
+            <>
+                <h1 className="text-center fw-bold mb-5 mt-5">Popular Movies</h1>
+                
+                <div className="container">
+                    <div className="row">
+                        {movies.map(movie => (
+                            <Card 
+                                poster={movie.poster_path}
+                                title={movie.title}
+                                releaseDate={movie.release_date}
+                                overview={movie.overview}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </>
         );
     }
 }
