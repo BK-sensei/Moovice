1d1c478873fb3d264b40952757060c30bf2b73cd
becca <rebecca.kaci@gmail.com>
2021-11-16 11:35:09 +0100

    composant Card terminé

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ class Card extends Component {
                     <img src={(`https://image.tmdb.org/t/p/w300/${poster}`)} class="card-img-top" alt="Movie Poster" />
                     <div class="card-body">
                         <h4 class="card-title text-center fw-bold mb-2">{title}</h4>
-                        <p class="card-text fst-italic text-center">Realised on the : {releaseDate}</p>
+                        <p class="card-text fst-italic text-center text-dark">Realised on the : {releaseDate}</p>
                         <p class="card-text"><span className="fw-bold">Description :</span> {overview}</p>
                     </div>
                 </div>
