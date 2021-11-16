616d8353ac0a114aeab37caca544ea0b1a217995
becca <rebecca.kaci@gmail.com>
2021-11-16 17:32:11 +0100

    commencement du composant Weekly

--- src/pages/Weekly.js
+++ src/pages/Weekly.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Card from '../components/Card'
+import moment from 'moment'
 
 class Weekly extends Component {
+    constructor() {
+        super()
+
+        // state initial
+        this.state = {
+            movies : []
+        }
+    }
+
+    componentDidMount(){
+        fetch(`hhttp://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${LAST_WEEK}&primary_release_date.lte=${TODAY}&api_key=74ff4d5b18f55c304a239fadf716fe2f`)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            this.setState({movies: result.results})
+        })
+        console.log(this.state.movies);
+    }
+
     render() {
+        console.log(this.state.movies);
+        const { movies } = this.state
+
         return (
-            <div>
-                <h1>Weekly</h1>
-            </div>
+            <>
+                <h1 className="text-center fw-bold mb-5 mt-5">Popular Movies</h1>
+                
+                <div className="container">
+                    <div className="row">
+                        {movies.map(movie => (
+                            <Card 
+                                key= {movie.title}
+                                poster={movie.poster_path}
+                                title={movie.title}
+                                releaseDate={movie.release_date}
+                                overview={movie.overview}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </>
         );
     }
 }
 
-export default Weekly;
+export default Weekly
